965f423cfb2c84e3d00daa89183cc91e49802810
sailingala-altiscale <sailingala@altiscale.com>
2020-03-28 13:45:54 -0700

    Adding google authentication

--- server.js
+++ server.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const path = require('path');
-
+const passport = require('passport');
+const auth = require('./googleAuth');
 const app = express();
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+auth(passport);
+app.use(passport.initialize());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['testKey']
+}));
+app.use(cookieParser());
+
+// Authentication Call
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+// Authntication Call back which is configured on google console
+app.get('/auth/google/callback',
+passport.authenticate('google', {
+    failureRedirect: '/error'
+}),
+(req, res) => {
+    req.session.token = req.user.token;
+    res.redirect('/');
+}
+);
+
+// Redirect to landing page after authentication
+app.get('/', (req, res) => {
+  if (req.session.token) {
+    console.log(JSON.stringify(req.session.passport.user.profile.displayName));
+    res.cookie('token', req.session.token);
+    app.use(express.static(path.join(__dirname, '/build')));
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+} else {
+    res.cookie('token', '')
+    res.json({
+        status: 'session cookie not set'
+    });
+}
+});
 
 // get all items
 app.get('/api/v1/items', (req, res) => {
@@ -16,10 +55,11 @@ app.get('/api/v1/items', (req, res) => {
   res.json({ hello: "world "});
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+// logout call TO-DO
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session = null;
+    res.redirect('/');
 });
 
 const port = process.env.PORT || 3001;
