ad4512edcee8e2d4e4f4d5416120d2461d97a8a3
Rohan Venapusala <ronvenna@gmail.com>
2020-03-29 20:27:07 -0700

    added the ability to show the item when user clicks

--- src/Items.js
+++ src/Items.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rodal from 'rodal';
 import Header from './Header';
 
 class Items extends React.Component {
@@ -8,34 +9,52 @@ class Items extends React.Component {
     this.state = {
       loading: true,
       items: [],
+      modalOpen: false,
+      selectedItem: null,
     }
+
+    this.showItem = this.showItem.bind(this);
+    this.hideItem = this.hideItem.bind(this);
     this.getItems = this.getItems.bind(this);
+    this.renderItem = this.renderItem.bind(this);
   }
 
   componentDidMount() {
     this.getItems();
   }
 
+  showItem(item) {
+    this.setState({ modalOpen: true, selectedItem: item });
+  }
+
+  hideItem() {
+    this.setState({ modalOpen: false, selectedItem: null });
+  }
+
   getItems() {
     this.setState({ loading: true });
-    fetch('/api/v1/items')
+    fetch('/api/v1/availableItems')
       .then((res) => res.json())
       .then((items) => this.setState(() => ({ items, loading: false })))
       .catch(console.error)
   }
 
-  renderImage(image) {
+  renderItem(item) {
     return (
-      <div className="item" key={image.itemid}>
+      <div
+        className="item"
+        key={item.itemid}
+        onClick={() => this.showItem(item)}
+      >
         <div className="item__title_container">
-          <p className="item__title"> {image.itemname} </p>
+          <p className="item__title"> {item.itemname} </p>
         </div>
         {
-          image.itemtype ? (
+          item.itemtype ? (
             <img
-              alt={image.itemname}
+              alt={item.itemname}
               className="item__image"
-              src={image.itemtype}
+              src={item.itemtype}
             />
         ) : null
         }
@@ -44,6 +63,12 @@ class Items extends React.Component {
   }
 
   render(){
+    const {
+      items,
+      selectedItem,
+    } = this.state;
+    console.log(selectedItem);
+    console.log(items);
     return (
       <div>
         <Header
@@ -65,11 +90,34 @@ class Items extends React.Component {
               (
                 <div className="items">
                   {
-                    this.state.items.map(this.renderImage)
+                    this.state.items.map(this.renderItem)
                   }
                 </div>
               )
         }
+        <Rodal visible={this.state.modalOpen} onClose={this.hideItem} animation="slideUp">
+          {
+            selectedItem && (
+              <div className="selected_item">
+                <h2> {selectedItem.itemname} </h2>
+                {
+                  selectedItem.itemtype && (
+                    <img
+                      alt={selectedItem.itemname}
+                      className="item__image"
+                      src={selectedItem.itemtype}
+                    />
+                  )
+                }
+                <div className="submitItem">
+                  <a href="mailto:user@example.com?subject=Subject&body=message%20goes%20here">
+                    <button className="postButton">Barter for item</button>
+                  </a>
+                </div>
+              </div>
+            )
+          }
+        </Rodal>
       </div>
     );
   }
